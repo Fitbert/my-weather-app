0c10fdee11d05e3c837246792d1bd213c62054fc
Albert T <albert.terrazas@gmail.com>
2024-01-31 15:18:52 -0600

    updated Js and CSS files
    put code for the app in the JS file. code is broken

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # my-weather-app
- My weatehr app using web api
+ My weather app using web api
+
+
+## Challenge Week 6
+
+My Weaterh app 
+
+
+
+## Usage
+
+The My Planner is for updating todo tasks for the work day. We are able to add daily tasks by the hour, as well as track the day with color.  
+
+[gif](./images/Work%20Day%20Scheduler.gif)
+./images/Screenshot 2024-01-30 145025.png
+https://fitbert.github.io/My-Daily-Planner/
+
+## Credits
+
+geeksforgeeks.com
+W3 Schools- general refrence
+scrimba
+mdm
+tyler potts youtube
+
+
+
+## Refection
+
+While continuing the work with json and DOM work we are able to manipulate the DOM with code that will follow time card. These are able to be stored on the local server to keep in the shell. As a starter code will be able to go back and continue with more features to make more interactive and visually appealing.
+
+
+## Features
+
+Save feature for todo items
+Notification the items where saved to the local server. 
+Clear button to reset the day. 
---
+++ weather.js
@@ -0,0 +1,51 @@
+const api ={
+    key: " 7ee985d3f3fcb82608bed4de2f5719c6",
+    base: "https://api.openweathermap.org/data/2.5/"
+}
+
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+
+
+function setQuery(evt){
+    if (evt.keyCode == 13) {
+        getResults(searchbox.value);
+    }
+}
+function getResults(query) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&APPID=7ee985d3f3fcb82608bed4de2f5719c6')
+    .then(weather => {
+        return weather.json();
+    }) .then(displayResults);
+    
+}
+function displayResults (weather) {
+    console.log(weather);
+    let city = document.elementSelector('.location . city');
+    city.innerText = '${weather.name}, ${weather.sys.country}';
+
+    let now =document.querySelector('.location .date');
+    temp.innerText = dateBuilder(now);
+
+    let tem = document.querySelector('.current .tem');
+    tem.innerHTML= '${Math.round(weather.main.temp)}<span>°c</span>';
+    let weather_el = document.querySelector('current . weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = '${Math.round(weather.main.temp_min)}'
+}
+
+function dateBuilder (d){
+    let months = ["January","Feb"]
+    let days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return '${day} ${date} ${month} ${year}';
+
+}
